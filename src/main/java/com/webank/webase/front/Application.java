07c0aa66ac71fc3983b84df39f289769e02b8528
shuoyanhe <yanyanho@126.com>
2019-04-08 17:14:32 +0800

    add annotation

--- src/main/java/com/webank/webase/front/Application.java
+++ src/main/java/com/webank/webase/front/Application.java
@@ -5,6 +5,7 @@ import org.fisco.bcos.web3j.precompile.cns.CnsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /*
@@ -29,6 +30,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Slf4j
 @EnableSwagger2
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
