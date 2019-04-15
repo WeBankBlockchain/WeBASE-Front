e677dd63d5609b07716405aa77726c2a745440e8
shuoyanhe <yanyanho@126.com>
2019-04-15 09:47:34 +0800

    merge the code

--- src/main/java/com/webank/webase/front/Application.java
+++ src/main/java/com/webank/webase/front/Application.java
@@ -32,7 +32,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableScheduling
 @SpringBootApplication
-@EnableScheduling
 public class Application {
 
     public static void main(String[] args) throws Exception {
