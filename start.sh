845bb5b208e7d8c76c98e7537d48ef233dec438a
shuoyanhe <yanyanho@126.com>
2019-04-24 14:31:29 +0800

    fix the readme of sigar

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ CONF_DIR=${CURRENT_DIR}/conf
 sigarFile="/usr/lib/libsigar-amd64-linux.so"
 
  if [ ! -f "sigarFile" ]; then
-  cp $CONF_DIR/libsigar-amd64-linux.so /usr/lib
+  sudo cp $CONF_DIR/libsigar-amd64-linux.so /usr/lib
  fi
 
 mkdir -p log
