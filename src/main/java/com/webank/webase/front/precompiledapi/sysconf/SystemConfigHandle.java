ea53e1a645083bcef1ebcfda3f5fe61c064ef3b5
CodingCattwo <847701726@qq.com>
2019-09-05 10:32:48 +0800

    delete sysconf db

--- src/main/java/com/webank/webase/front/precompiledapi/sysconf/SystemConfigHandle.java
+++ src/main/java/com/webank/webase/front/precompiledapi/sysconf/SystemConfigHandle.java
@@ -1,4 +1,4 @@
-package com.webank.webase.front.precompiledapi.sysconf.entity;
+package com.webank.webase.front.precompiledapi.sysconf;
 
 import com.webank.webase.front.base.ConstantCode;
 import lombok.Data;
@@ -7,21 +7,11 @@ import org.hibernate.validator.constraints.NotBlank;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-
-@Entity
-@Table(uniqueConstraints = {
-        @UniqueConstraint(name = "unique_sys_config", columnNames = {"groupId", "fromAddress",
-                "configKey", "configValue"})
-})
 @Data
-public class SystemConfig {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class SystemConfigHandle {
 
     @NotNull(message = ConstantCode.PARAM_FAIL_GROUPID_IS_EMPTY)
     private int groupId;
-//    @JsonProperty()
     @NotNull(message = ConstantCode.PARAM_FAIL_FROM_IS_EMPTY)
     private String fromAddress;
     @NotBlank(message = ConstantCode.PARAM_FAIL_CONFIG_KEY_IS_EMPTY)
