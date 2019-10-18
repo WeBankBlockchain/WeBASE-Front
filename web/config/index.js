f434a144e7c8c1ad8f00d4b94276479cfe951adf
xyzshen <304565162@qq.com>
2019-10-18 18:01:01 +0800

    modify  ip
    Signed-off-by: xyzshen <304565162@qq.com>

--- web/config/index.js
+++ web/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/handle':{
-                target:'http://10.58.2.53:5002/',
+                target:'http://127.0.0.1/',
                 changeOrigin:true,
                 pathRewrite:{
                     '^/handle':'WeBASE-Front/'
