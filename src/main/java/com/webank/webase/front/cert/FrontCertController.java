9bb3166ad28049ca2abc7ae183bee23c2254681a
CodingCattwo <847701726@qq.com>
2019-12-18 15:30:30 +0800

    add sdk cert

--- src/main/java/com/webank/webase/front/cert/FrontCertController.java
+++ src/main/java/com/webank/webase/front/cert/FrontCertController.java
@@ -56,14 +56,17 @@ public class FrontCertController {
         String chainCertStr;
         String nodeCrtStr;
         String agencyCrtStr;
-        String sdkCrtStr;
+        String sdkChainCrtStr;
+        String sdkAgencyCrtStr;
+        String sdkNodeCrtStr;
         // node的crt文件可能包含节点、机构、链证书三个
         // sdk的node.crt文件一般包含sdk节点证书，机构证书两个
         try {
+            sdkCertList = certService.getSDKNodeCert();
             nodeCertList = certService.getNodeCerts();
             chainCertStr = certService.getChainCert();
-            sdkCertList = certService.getSDKNodeCert();
         }catch (FrontException e) {
+            log.error("FrontCertController load cert error: e:[]", e);
             return new BaseResponse(ConstantCode.CERT_FILE_NOT_FOUND, e.getMessage());
         }
         Map<String, String> map = new HashMap<>();
@@ -71,7 +74,7 @@ public class FrontCertController {
         if(!StringUtils.isEmpty(chainCertStr)) {
             map.put("chain", chainCertStr);
         }
-        // node.crt
+        // node.crt agency.crt
         nodeCrtStr = nodeCertList.get(0);
         agencyCrtStr = nodeCertList.get(1);
         // 链证书重复：ca.crt = nodeCertList.get(2);
@@ -81,17 +84,28 @@ public class FrontCertController {
         if(!StringUtils.isEmpty(agencyCrtStr)) {
             map.put("agency", agencyCrtStr);
         }
-        // guomi: encrypt cert included: 3 normal cert, 1 encrypt cert
+
+        // guomi version: encrypt cert included: 3 normal cert, additional 1 encrypt cert
         if(nodeCertList.size() == 4){
-            String enNodeCrtStr = nodeCertList.get(3);
+            String enNodeCrtStr = nodeCertList.get(nodeCertList.size() - 1);
             if(!StringUtils.isEmpty(enNodeCrtStr)) {
                 map.put("ennode", enNodeCrtStr);
             }
         }
-        // sdk.crt
-        sdkCrtStr = sdkCertList.get(0);
-        if(!StringUtils.isEmpty(sdkCrtStr)) {
-            map.put("sdk", sdkCrtStr);
+        // sdk's node.crt (sdk.crt)
+        sdkChainCrtStr = sdkCertList.get(0);
+        if(!StringUtils.isEmpty(sdkChainCrtStr)) {
+            map.put("sdkca", sdkChainCrtStr);
+        }
+        // sdk's node.crt (sdk.crt)
+        sdkAgencyCrtStr = sdkCertList.get(1);
+        if(!StringUtils.isEmpty(sdkAgencyCrtStr)) {
+            map.put("sdkagency", sdkAgencyCrtStr);
+        }
+        // sdk's node.crt (sdk.crt)
+        sdkNodeCrtStr = sdkCertList.get(2);
+        if(!StringUtils.isEmpty(sdkNodeCrtStr)) {
+            map.put("sdknode", sdkNodeCrtStr);
         }
         log.info("end getFrontCerts. startTime:{}, certMap:{}",
                 Duration.between(startTime, Instant.now()).toMillis(), map);
