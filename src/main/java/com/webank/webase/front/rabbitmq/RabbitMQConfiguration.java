ff09f9b01c93d5060c8e9bb60ec8936b09cf0de0
yanyanho <yanyanho@126.com>
2020-01-16 11:19:40 +0800

    fix the code style

--- src/main/java/com/webank/webase/front/rabbitmq/RabbitMQConfiguration.java
+++ src/main/java/com/webank/webase/front/rabbitmq/RabbitMQConfiguration.java
@@ -19,7 +19,7 @@ import java.util.Map;
 @EnableConfigurationProperties(RabbitMQProperties.class)
 public class RabbitMQConfiguration {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+    public final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private AmqpAdmin rabbitAdmin;
