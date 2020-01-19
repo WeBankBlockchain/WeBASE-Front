6a7e833d7972f62fed3738a1dfcc1314b1c95799
CodingCattwo <847701726@qq.com>
2020-01-19 16:56:13 +0800

    add license

--- src/main/java/com/webank/webase/front/rabbitmq/RabbitMQPublisher.java
+++ src/main/java/com/webank/webase/front/rabbitmq/RabbitMQPublisher.java
@@ -1,6 +1,5 @@
 package com.webank.webase.front.rabbitmq;
 
-import com.webank.webase.front.rabbitmq.MqObject;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
