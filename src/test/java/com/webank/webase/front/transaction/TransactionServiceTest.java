d549607006d43cb3ffb4d6382601a1224a8ebff5
CodingCattwo <847701726@qq.com>
2020-03-03 12:17:27 +0800

    add precompiled test

--- src/test/java/com/webank/webase/front/transaction/TransactionServiceTest.java
+++ src/test/java/com/webank/webase/front/transaction/TransactionServiceTest.java
@@ -18,6 +18,8 @@ package com.webank.webase.front.transaction;
 
 import com.alibaba.fastjson.JSON;
 import com.webank.webase.front.Application;
+import com.webank.webase.front.base.enums.PrecompiledTypes;
+import com.webank.webase.front.precompiledapi.PrecompiledCommonInfo;
 import com.webank.webase.front.transaction.entity.ReqTransHandleWithSign;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -50,4 +52,10 @@ public class TransactionServiceTest {
 //        Object res = transService.transHandleWithSign(param);
 //        System.out.println(res);
 //    }
+
+    @Test
+    public void testTransPrecompiledWithSign() {
+//        transService.transHandleWithSignForPrecompile(1, 100001,
+//        PrecompiledTypes.SYSTEM_CONFIG, PrecompiledCommonInfo.FUNC_SETVALUEBYKEY, )
+    }
 }
