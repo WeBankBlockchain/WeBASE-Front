089cbd3457d8125de8ddf81e14e84925597608e7
CodingCattwo <847701726@qq.com>
2020-03-10 18:22:18 +0800

    rm duplicated code

--- src/main/java/com/webank/webase/front/event/EventService.java
+++ src/main/java/com/webank/webase/front/event/EventService.java
@@ -33,6 +33,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
