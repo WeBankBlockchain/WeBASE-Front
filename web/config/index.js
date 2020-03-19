19451dc9b2220df622a8c3e73a90272476d34548
wagonghao <vaststarsea@foxmail.com>
2020-03-19 15:06:11 +0800

    fix bug

--- web/config/index.js
+++ web/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/handle':{
-                target:'http://10.107.105.138:5902',
+                target:'http://127.0.0.1',
                 changeOrigin:true,
                 pathRewrite:{
                     '^/handle':'WeBASE-Front/'
