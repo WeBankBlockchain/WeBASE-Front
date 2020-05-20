14189d2c809bb4601bd487fdc3ed81611f5f553c
CodingCattwo <847701726@qq.com>
2020-05-20 14:59:54 +0800

    optimize file server of classpath

--- src/main/java/com/webank/webase/front/base/config/WebMvcConfiguration.java
+++ src/main/java/com/webank/webase/front/base/config/WebMvcConfiguration.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.io.File;
-
-import static com.webank.webase.front.solc.SolcService.SOLC_DIR_PATH;
 
 @Slf4j
 @Configuration
@@ -15,17 +12,8 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 前端浏览器访问路径带有/file/**就转到对应磁盘下读取js文件,
-        // 类似前端访问tomcat webapp下file文件夹中文件
-        File fileDir = new File(SOLC_DIR_PATH);
-        // check parent path
-        if(!fileDir.exists()){
-            // 递归生成文件夹
-            fileDir.mkdirs();
-        }
-        String filePath = fileDir.getAbsolutePath() + File.separator;
-        log.info("addResourceHandlers path:{}", filePath);
-        registry.addResourceHandler("/file/**").addResourceLocations("file:" + filePath);
+        // web request uri of "/solcjs/**", return the files in classpath/solcjs/xx.js
+        registry.addResourceHandler("/solcjs/**").addResourceLocations("classpath:/solcjs/");
         super.addResourceHandlers(registry);
     }
 }
