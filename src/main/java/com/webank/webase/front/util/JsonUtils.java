9f5662c97cd2f63d563962a8678567196ddae07d
CodingCattwo <847701726@qq.com>
2020-06-15 11:49:10 +0800

    add jackson util

---
+++ src/main/java/com/webank/webase/front/util/JsonUtils.java
@@ -0,0 +1,249 @@
+package com.webank.webase.front.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+@Slf4j
+public class JsonUtils {
+    // 加载速度太慢了，放在静态代码块中
+    // private static final ObjectMapper mapper = new ObjectMapper();
+    private static ObjectMapper mapper;
+    private static final String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    /**
+     * 设置一些通用的属性
+     */
+    static {
+        mapper = new ObjectMapper();
+        // 如果json中有新增的字段并且是实体类类中不存在的，不报错
+        // mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+        // 如果存在未知属性，则忽略不报错
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        // 允许key没有双引号
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        // 允许key有单引号
+        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        // 允许整数以0开头
+        mapper.configure(JsonParser.Feature.ALLOW_NUMERIC_LEADING_ZEROS, true);
+        // 允许字符串中存在回车换行控制符
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+        // 属性值为null的不参与序列化
+//        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+        // timestamp
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.setDateFormat(new SimpleDateFormat(STANDARD_FORMAT));
+    }
+
+    public static String toJSONString(Object obj) {
+        return obj != null ? toJSONString(obj, () -> "") : "";
+    }
+
+    public static String toJSONString(Object obj, Supplier<String> defaultSupplier) {
+        try {
+            return obj != null ? mapper.writeValueAsString(obj) : defaultSupplier.get();
+        } catch (Throwable e) {
+            log.error(String.format("toJSONString %s", obj != null ? obj.toString() : "null"), e);
+        }
+        return defaultSupplier.get();
+    }
+
+    public static <T> T toJavaObject(String value, Class<T> tClass) {
+        return StringUtils.isNotBlank(value) ? toJavaObject(value, tClass, () -> null) : null;
+    }
+
+    public static <T> T toJavaObject(Object obj, Class<T> tClass) {
+        return obj != null ? toJavaObject(toJSONString(obj), tClass, () -> null) : null;
+    }
+
+    public static <T> T toJavaObject(String value, Class<T> tClass, Supplier<T> defaultSupplier) {
+        try {
+            if (StringUtils.isBlank(value)) {
+                return defaultSupplier.get();
+            }
+            return mapper.readValue(value, tClass);
+        } catch (Throwable e) {
+            log.error(String.format("toJavaObject exception: \n %s\n %s", value, tClass), e);
+        }
+        return defaultSupplier.get();
+    }
+
+    public static <T> List<T> toJavaObjectList(String value, Class<T> tClass) {
+        return StringUtils.isNotBlank(value) ? toJavaObjectList(value, tClass, () -> null) : null;
+    }
+
+    public static <T> List<T> toJavaObjectList(Object obj, Class<T> tClass) {
+        return obj != null ? toJavaObjectList(toJSONString(obj), tClass, () -> null) : null;
+    }
+
+    public static <T> List<T> toJavaObjectList(String value, Class<T> tClass, Supplier<List<T>> defaultSupplier) {
+        try {
+            if (StringUtils.isBlank(value)) {
+                return defaultSupplier.get();
+            }
+            JavaType javaType = mapper.getTypeFactory().constructParametricType(List.class, tClass);
+            return mapper.readValue(value, javaType);
+        } catch (Throwable e) {
+            log.error(String.format("toJavaObjectList exception \n%s\n%s", value, tClass), e);
+        }
+        return defaultSupplier.get();
+    }
+
+    // 简单地直接用json复制或者转换(Cloneable)
+    public static <T> T jsonCopy(Object obj, Class<T> tClass) {
+        return obj != null ? toJavaObject(toJSONString(obj), tClass) : null;
+    }
+
+    public static Map<String, Object> toMap(String value) {
+        return StringUtils.isNotBlank(value) ? toMap(value, () -> null) : null;
+    }
+
+    public static Map<String, Object> toMap(Object value) {
+        return value != null ? toMap(value, () -> null) : null;
+    }
+
+    public static Map<String, Object> toMap(Object value, Supplier<Map<String, Object>> defaultSupplier) {
+        if (value == null) {
+            return defaultSupplier.get();
+        }
+        try {
+            if (value instanceof Map) {
+                return (Map<String, Object>) value;
+            }
+        } catch (Exception e) {
+            log.error("fail to convert" + toJSONString(value), e);
+        }
+        return toMap(toJSONString(value), defaultSupplier);
+    }
+
+    public static Map<String, Object> toMap(String value, Supplier<Map<String, Object>> defaultSupplier) {
+        if (StringUtils.isBlank(value)) {
+            return defaultSupplier.get();
+        }
+        try {
+            return toJavaObject(value, LinkedHashMap.class);
+        } catch (Exception e) {
+            log.error(String.format("toMap exception\n%s", value), e);
+        }
+        return defaultSupplier.get();
+    }
+
+
+    public static List<Object> toList(String value) {
+        return StringUtils.isNotBlank(value) ? toList(value, () -> null) : null;
+    }
+
+    public static List<Object> toList(Object value) {
+        return value != null ? toList(value, () -> null) : null;
+    }
+
+    public static List<Object> toList(String value, Supplier<List<Object>> defaultSuppler) {
+        if (StringUtils.isBlank(value)) {
+            return defaultSuppler.get();
+        }
+        try {
+            return toJavaObject(value, List.class);
+        } catch (Exception e) {
+            log.error("toList exception\n" + value, e);
+        }
+        return defaultSuppler.get();
+    }
+
+    public static List<Object> toList(Object value, Supplier<List<Object>> defaultSuppler) {
+        if (value == null) {
+            return defaultSuppler.get();
+        }
+        if (value instanceof List) {
+            return (List<Object>) value;
+        }
+        return toList(toJSONString(value), defaultSuppler);
+    }
+
+    /* author: clk */
+
+    public static boolean isJson(String str) {
+        try {
+            mapper.readTree(str);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public static JsonNode stringToJsonNode(String str) {
+        try {
+            return mapper.readTree(str);
+        } catch (IOException e) {
+            log.error("Parse String to JsonNode error : {}", e.getMessage());
+            return null;
+        }
+    }
+
+    public static <T> String objToString(T obj) {
+        if (obj == null) {
+            return null;
+        }
+        try {
+            return obj instanceof String ? (String) obj
+                : mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            log.error("Parse Object to String error : {}", e.getMessage());
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T stringToObj(String str, Class<T> clazz) {
+        if (StringUtils.isEmpty(str) || clazz == null) {
+            return null;
+        }
+        try {
+            return clazz.equals(String.class) ? (T) str : mapper.readValue(str, clazz);
+        } catch (Exception e) {
+            log.error("Parse String to Object error : {}", e.getMessage());
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T stringToObj(String str, TypeReference<T> typeReference) {
+        if (StringUtils.isEmpty(str) || typeReference == null) {
+            return null;
+        }
+        try {
+            return (T) (typeReference.getType().equals(String.class) ? str
+                : mapper.readValue(str, typeReference));
+        } catch (IOException e) {
+            log.error("Parse String to Object error", e);
+            return null;
+        }
+    }
+
+    public static <T> T stringToObj(String str, Class<?> collectionClazz,
+        Class<?>... elementClazzes) {
+        JavaType javaType = mapper.getTypeFactory()
+            .constructParametricType(collectionClazz, elementClazzes);
+        try {
+            return mapper.readValue(str, javaType);
+        } catch (IOException e) {
+            log.error("Parse String to Object error : {}" + e.getMessage());
+            return null;
+        }
+    }
+}
