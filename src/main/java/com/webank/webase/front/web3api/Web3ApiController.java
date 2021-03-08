4b91e5ba0eb905af9b696035d26ece108b6e94f6
CodingCattwo <847701726@qq.com>
2021-03-08 16:19:15 +0800

    add rsp of stat block

--- src/main/java/com/webank/webase/front/web3api/Web3ApiController.java
+++ src/main/java/com/webank/webase/front/web3api/Web3ApiController.java
@@ -34,7 +34,6 @@ import org.fisco.bcos.sdk.client.protocol.response.BcosBlockHeader;
 import org.fisco.bcos.sdk.client.protocol.response.ConsensusStatus.ConsensusInfo;
 import org.fisco.bcos.sdk.client.protocol.response.Peers;
 import org.fisco.bcos.sdk.client.protocol.response.SyncStatus.SyncStatusInfo;
-import org.fisco.bcos.sdk.client.protocol.response.TotalTransactionCount;
 import org.fisco.bcos.sdk.model.NodeVersion.ClientVersion;
 import org.fisco.bcos.sdk.model.TransactionReceipt;
 import org.springframework.beans.factory.annotation.Autowired;
