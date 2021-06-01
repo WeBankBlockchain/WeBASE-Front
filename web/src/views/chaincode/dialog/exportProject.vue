51a39e142d6bd7ff229dc2dcc6522c4bd5f89511
xyzshen <304565162@qq.com>
2021-06-01 12:43:08 +0800

    optimize
    Signed-off-by: xyzshen <304565162@qq.com>

--- web/src/views/chaincode/dialog/exportProject.vue
+++ web/src/views/chaincode/dialog/exportProject.vue
@@ -388,6 +388,10 @@ export default {
                     } else {
                         navigator.msSaveBlob(blob, fileName)
                     }
+                    this.$message({
+                        type: 'success',
+                        message: this.$t('text.exportSuccessed')
+                    })
                 }
             })
         },
