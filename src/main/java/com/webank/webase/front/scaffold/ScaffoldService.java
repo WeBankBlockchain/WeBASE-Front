1dbeee1d181fed09c2d4bce3315b3fd51472c2e7
CodingCattwo <847701726@qq.com>
2021-08-11 11:43:57 +0800

    fix channel port

--- src/main/java/com/webank/webase/front/scaffold/ScaffoldService.java
+++ src/main/java/com/webank/webase/front/scaffold/ScaffoldService.java
@@ -100,6 +100,14 @@ public class ScaffoldService {
         NodeConfig thisConfig = new NodeConfig();
         BeanUtils.copyProperties(nodeConfig, thisConfig);
         thisConfig.setP2pip(reqProject.getChannelIp());
+        // set port, if param not null, set as param's port
+        // if param null, response from front not null, set as response's port
+        // else all null, set as 20200 default value
+        if (reqProject.getChannelPort() != null) {
+            thisConfig.setChannelPort(String.valueOf(reqProject.getChannelPort()));
+        } else if (StringUtils.isBlank(nodeConfig.getChannelPort())) {
+            thisConfig.setChannelPort("20200");
+        }
         log.info("exportProject get thisConfig:{}", thisConfig);
         // get front's sdk key cert
         Map<String, String> sdkMap = certService.getSDKCertKeyMap();
