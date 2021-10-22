f2aca1b433353dc48a992d40ae61e4804aebbcc3
CodingCattwo <847701726@qq.com>
2021-10-22 16:37:52 +0800

    rm throw exception

--- src/main/java/com/webank/webase/front/contractStore/PresetDataService.java
+++ src/main/java/com/webank/webase/front/contractStore/PresetDataService.java
@@ -383,6 +383,7 @@ public class PresetDataService {
             return jsonStr;
         } catch (Exception e) {
             log.error("loadWarehouseJson error:[]", e);
+            return null;
         }
     }
 }
