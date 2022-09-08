49a5a42da8df539871000fba7145171888d59e18
“jinzhiwen” <724338817@qq.com>
2022-09-08 17:12:25 +0800

    repairUpload

--- web/src/views/chaincode/components/contractCatalog.vue
+++ web/src/views/chaincode/components/contractCatalog.vue
@@ -23,7 +23,7 @@
         <i class="wbs-icon-Addfolder icon contract-icon" @click="addFolder"></i>
       </el-tooltip>
       <el-tooltip class="item" effect="dark" :content="$t('title.upload')" placement="top-start">
-        <i class="wbs-icon-shangchuan contract-icon" style="position: relative">
+        <i class="wbs-icon-shangchuan contract-icon" style="position: relative"  @click="uploading">
           <input multiple type="file" id="file" ref="file" name="chaincodes" class="uploads" @change="upload($event)" />
         </i>
       </el-tooltip>
@@ -1395,6 +1395,9 @@ export default {
       this.importFromDialog = false;
       this.getContractPaths();
     },
+     uploading(){
+     this.$refs.file.click()   
+    }
   },
 };
 </script>
@@ -1445,8 +1448,8 @@ export default {
 }
 .uploads {
   position: absolute;
-  width: 18px;
-  height: 18px;
+  width: 0px;
+  height: 0px;
   left: 10px;
   top: 0;
   opacity: 0;
