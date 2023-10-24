62840c0ccd224927116063b98a5178d52d64005b
yyzhangyang <zy871029@126.com>
2023-10-24 11:02:16 +0800

    feat : 解决没有拦截到请求信息的问题

--- src/main/java/com/webank/webase/front/base/config/WebMvcConfig.java
+++ src/main/java/com/webank/webase/front/base/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.webank.webase.front.base.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +15,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public NodeReqInterceptor nodeReqInterceptor() {
         return new NodeReqInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(nodeReqInterceptor()).addPathPatterns("/**");
     }
 
     /**
